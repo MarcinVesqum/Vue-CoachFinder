d60c8eb19f5a0208de3df260160e7c40a4de4987
Vesqum <marcin.pelszyk30@gmail.com>
2022-07-15 10:21:46 +0200

    added fetching coach data (GET http requests)

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -1,3 +1,50 @@
 <template>
-    requests
+    <base-card>
+        <header>
+            <h2>Requests</h2>
+        </header>
+        <ul v-if="hasRequests">
+            <request-item 
+            v-for="req in receivedRequests" 
+            :key="req.id"
+            :id="req.id"
+            :email="req.userEmail"
+            :message="req.message"
+
+            ></request-item>
+        </ul>
+        <h3 v-else>You haven't received any requests yet!</h3>
+    </base-card>
 </template>
+
+<script>
+import RequestItem from '../../components/requests/RequestItem.vue'
+export default {
+    computed: {
+        receivedRequests() {
+            return this.$store.getters["requests/requests"];
+        },
+        hasRequests() {
+            return this.$store.getters['requests/hasRequests']
+        }
+    },
+    components: { RequestItem }
+}
+</script>
+
+<style scoped>
+header {
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
+</style>
